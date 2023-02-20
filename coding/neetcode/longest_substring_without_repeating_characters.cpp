a0ef1d7f8c6c83c91f73f295d58cbe7d8dbef747
jatzz10 <jatinsharma2751997@gmail.com>
2023-02-20 09:48:07 +0530

    Minor change

--- coding/neetcode/longest_substring_without_repeating_characters.cpp
+++ coding/neetcode/longest_substring_without_repeating_characters.cpp
@@ -2,7 +2,7 @@
 
 // Solutions
 
-// Approach 1
+// Approach 1: Brute Force
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -36,21 +36,45 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         map<char, int> mp;
-        int count = 0;
+        int l = 0, r = 0, maxCount = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            mp[s[i]]++;
-            
-            if (mp[s[i]] > 1) {
-                count = mp.size() > count ? mp.size() : count;
-                mp.clear();
-                mp[s[i]]++;
+        while (r < s.size()) {
+            mp[s[r]]++;
+
+            while (mp[s[r]] > 1) {
+                mp[s[l]]--;
+                l++;
             }
+
+            maxCount = max(maxCount, r-l+1);
+            r++;
         }
 
-        count = mp.size() > count ? mp.size() : count;
+        return maxCount;
+    }
+};
+// Time complexity - O(2n) since each charater will be visited atleast twice
+// Space complexity - O(n)
+// Auxiliary space - O(n)
 
-        return count;
+
+// Approach 3: Slider Window Optimized
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char, int> mp;
+        int l = 0, r = 0, maxCount = 0;
+
+        while (r < s.size()) {
+            if (mp[s[r]] > 0) {
+                l = max(l, mp[s[r]]);
+            }
+            mp[s[r]] = r+1;
+            maxCount = max(maxCount, r-l+1);
+            r++;
+        }
+
+        return maxCount;
     }
 };
 // Time complexity - O(n)
