bef7c811df1ee6527f860deb2a7bf0f326540aff
jatzz10 <jatinsharma2751997@gmail.com>
2023-04-01 17:24:09 +0530

    Add neetcode problem

---
+++ coding/neetcode/reverse_bits.cpp
@@ -0,0 +1,26 @@
+// Problem - 
+
+// Solution
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        vector<int> bits(32, 0);
+        int counter = 0;
+
+        while (n) {
+            bits[counter++] = n&1;
+            n = n >> 1;
+        }
+
+        counter = 0;
+        for (int idx = bits.size()-1; idx >= 0; idx--) {
+            result += bits[idx]*pow(2, counter++);
+        }
+
+        return result;
+    }
+};
+// Time complexity - log(N)
+// Space complexity - O(1)
+// Auxiliary space - O(1)
